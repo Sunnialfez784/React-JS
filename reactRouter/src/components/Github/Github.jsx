8f0fa098d06859449006303da5e2011573d0fbf2
Sunni Alfez <sunnialfez784@gmail.com>
2026-02-26 15:38:49 +0530

    react Router

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+  const data = useLoaderData()
+  // const [data , setData] = useState([])
+  // useEffect(() =>{
+  //   fetch("https://api.github.com/users/Sunnialfez784").then((res) => res.json()).then((data) => setData(data))
+  // },[])
+  return (
+    <div className='text-center m-4 bg-gray-300 text-black p-4 text-3xl'>
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt="Github picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async ()=> {
+  const response = await fetch("https://api.github.com/users/Sunnialfez784")
+  return response.json();
+}
