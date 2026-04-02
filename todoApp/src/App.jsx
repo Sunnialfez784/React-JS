563635a673a1f9d556104ffdad15644db14db5de
Sunni Alfez <sunnialfez784@gmail.com>
2026-04-02 17:00:15 +0530

    todoApp

---
+++ todoApp/src/App.jsx
@@ -0,0 +1,31 @@
+import {useTodo} from "./context";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
+
+
+function App() {
+const {todos} = useTodo()
+  
+  return (
+   
+      <div className="bg-[#313337] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-2xl rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {Array.isArray(todos) && todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+  );
+}
+
+export default App;
---
+++ todoApp/src/context/ContextProvider.jsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from "react";
+import {TodoProvider} from "./TodoContext";
+
+const ContextProvider = ({children}) => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    console.log(todo, "todo inside");
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev]);
+  };
+  console.log(todos);
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => {
+      return prev.filter((prevTodo) => prevTodo.id !== id);
+    });
+  };
+
+  const toggleComplete = (id) => {
+    const allTodos = [...todos];
+    const findUpdateItemIndex = allTodos.findIndex((p) => p.id === id);
+
+    allTodos[findUpdateItemIndex] = {
+      ...allTodos[findUpdateItemIndex],
+      completed: !allTodos[findUpdateItemIndex].completed,
+    };
+
+    setTodos(allTodos);
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    {children}
+    </TodoProvider>;
+};
+
+export default ContextProvider;
